cf1a9413bebfce088558045a220e1917901518b0
Amr Kharroub <amrkharroub@gmail.com>
2025-11-09 21:19:20 +0200

    finished the backend review service

---
+++ backend/src/schemas/schemas.php
@@ -0,0 +1,17 @@
+<?php
+include("./backend/config/config.php");
+
+$SCHEMA = ["type" => "object"];
+$SCHEMA["properties"] = [
+    "severity" => ["type" => "string", "enum" => $ALLOWED_SEVERITIES],
+    "file" => ["type" => "string", "description" => "file name where the issue was found, it can also be 'string' if code was given as a string"],
+    "issue" => ["type" => "string", "description" => "Description of the code issue if found", "minLength" => 1],
+    "suggestion" => ["type" => "string", "description" => "Specific suggestion to fix the issue found", "minLength" => 1],
+    "line" => ["type" => ["integer", "null"], "minLength" => 1, "description" => "Optional, line number where the issue was found"],
+    "rule_id" => ["type" => ["string", "null"], "description" => "Optional unique identifier for the rule violated"],
+    "category" => ["type" => ["string", "null"], "enum" => ["security", "performance", "best-practice", "maintainability", "readability", "bug-risk"]]
+];
+$SCHEMA["required"] = ["severity", "file", "issue", "suggestion", "line", "rule_id", "category"];
+$SCHEMA["additionalProperties"] = false;
+
+?>
---
+++ backend/src/services/fileService.php
@@ -0,0 +1,39 @@
+<?php
+include("./backend/config/config.php");
+
+function process_upload(array $file){
+    global $MAX_FILE_SIZE, $UPLOAD_DIR, $ALLOWED_FILE_TYPES;
+
+    if($file["error"] === UPLOAD_ERR_OK){
+        throw new Exception("File upload error {$file['error']}");
+    }
+
+    if($file["size"] > $MAX_FILE_SIZE){
+        $file_info["error"] = 1;
+        return $file_info;
+    }
+
+    $file_ext = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+    
+    if(!empty($ALLOWED_FILE_TYPES) && !in_array($file_ext, $ALLOWED_FILE_TYPES)){
+        throw new Exception("File Type is not allowed {$file_ext}");
+    }
+
+    if(!file_exists($UPLOAD_DIR)){
+        mkdir($UPLOAD_DIR, 0755, true);
+    }
+    $safe_name = uniqid("{$file['name']}_", false) . "." . $file_ext;
+    $destination = $UPLOAD_DIR . "/" . $safe_name;
+
+    if(!move_uploaded_file($file["temp_name"], $destination)){
+        throw new Exception("Failed to move uploaded file");
+    }
+    
+    return [
+        "filename" => $file["name"],
+        "file_path" => $destination,
+        "language" => $file_ext,
+    ];
+}
+
+?>
