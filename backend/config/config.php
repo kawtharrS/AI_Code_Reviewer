293601e5b6eb832c59c307431a7580083e8fc1f8
Kawthar Shaalan <101220311@mu.edu.lb>
2025-11-10 02:13:03 +0200

    fixing api key

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,9 +1,10 @@
 <?php
-$API_KEY = "./key.txt";
+$env = parse_ini_file(".env"); 
+$API_KEY = $env['OPENAI_KEY'] ?? '';
 $URL_OPENAI = "https://api.openai.com/v1/responses";
 $STEP_TYPE = "short"; // short | long | persice | helpful etc..
 $ALLOWED_SEVERITIES = ["high", "medium", "low"];
-$MODEL_VERSION = "gpt-5-mini-2025-08-07";
+$MODEL_VERSION = "gpt-4o-mini";
 $ALLOWED_FILE_TYPES = []; //empty = all file types
 $MAX_FILE_SIZE = 5 * 1024 * 1024; //size in bytes
 $UPLOAD_DIR = dirname(__DIR__) . "/uploads";
